fcb595a96996d7a887915eb97c37b7ba8cb32d2a
baongocmai <oreooreo197@gmail.com>
2025-11-13 22:28:48 +0700

    u

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const path = require('path');
 const XLSX = require('xlsx');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 // Folder chứa file Excel
 const dataDir = path.join(__dirname, 'Transaction Sales Data');
@@ -37,5 +38,5 @@ app.get('/api/excel', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server chạy tại http://localhost:${port}`);
+  console.log(`Server chạy tại cổng ${port}`);
 });
